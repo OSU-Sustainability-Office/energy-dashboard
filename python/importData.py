936468853e3703ad8b508cb2403be3233768fba6
Brogan Miner <BroganMiner22@gmail.com>
2018-05-19 20:44:04 -0700

    uncommented python

--- python/importData.py
+++ python/importData.py
@@ -26,9 +26,7 @@ if not sql.fetchone():
 	for building in db.buildings.find({}):
 		sql.execute('INSERT INTO buildings (Name,sus_map_id) VALUES ("%s","%s")'%(building['name'],''))
 
-# sql.execute('SELECT * FROM buildings')
-# for row in sql.fetchall():
-# 	print str(row[0]) + " " + str(row[1]) + " " + str(row[2])
+
 
 sql.execute('SHOW TABLES LIKE "meters"')
 if not sql.fetchone():
@@ -51,13 +49,11 @@ if not sql.fetchone():
 	sql.execute('CREATE TABLE data (id int NOT NULL AUTO_INCREMENT, building_id int, time TIMESTAMP, accumulated_real int, real_power float, reactive_power float, apparent_power float, real_a float, real_b float, real_c float, reactive_a float, reactive_b float, reactive_c float, apparent_a float, apparent_b float, apparent_c float, pf_a float, pf_b float, pf_c float, vphase_ab float, vphase_bc float, vphase_ac float, vphase_an float, vphase_bn float, vphase_cn float, cphase_a float, cphase_b float, cphase_c float, PRIMARY KEY (id), FOREIGN KEY (building_id) REFERENCES buildings(id))')
 	#table population script 
 
-# sql.execute('SHOW TABLES LIKE "temp_data"')
-# if not sql.fetchone():
-# 	sql.execute('CREATE TABLE temp_data (id int NOT NULL AUTO_INCREMENT, meter_address CHAR(16), time TIMESTAMP, accumulated_real int, real_power float, reactive_power float, apparent_power float, real_a float, real_b float, real_c float, reactive_a float, reactive_b float, reactive_c float, apparent_a float, apparent_b float, apparent_c float, pf_a float, pf_b float, pf_c float, vphase_ab float, vphase_bc float, vphase_ac float, vphase_an float, vphase_bn float, vphase_cn float, cphase_a float, cphase_b float, cphase_c float, PRIMARY KEY (id))')
+sql.execute('SHOW TABLES LIKE "temp_data"')
+if not sql.fetchone():
+ 	sql.execute('CREATE TABLE temp_data (id int NOT NULL AUTO_INCREMENT, meter_address CHAR(16), time TIMESTAMP, accumulated_real int, real_power float, reactive_power float, apparent_power float, real_a float, real_b float, real_c float, reactive_a float, reactive_b float, reactive_c float, apparent_a float, apparent_b float, apparent_c float, pf_a float, pf_b float, pf_c float, vphase_ab float, vphase_bc float, vphase_ac float, vphase_an float, vphase_bn float, vphase_cn float, cphase_a float, cphase_b float, cphase_c float, PRIMARY KEY (id))')
 for entry in db.dataentries.find({}).sort([( 'timestamp', pymongo.DESCENDING )]).limit(5000):
 	meter = db.meters.find_one({ '_id' : entry['meter_id']})
-	# sql.execute('SELECT id FROM meters WHERE address = "%s"'%(meter['meter_id']))
-	# for sid in sql.fetchall():
 	if len(entry['point']) == 72:
 		sql.execute('INSERT INTO temp_data (meter_address, time, accumulated_real, real_power, reactive_power, apparent_power, real_a, real_b, real_c, reactive_a, reactive_b, reactive_c, apparent_a, apparent_b, apparent_c, pf_a, pf_b, pf_c, vphase_ab, vphase_bc, vphase_ac, vphase_an, vphase_bn, vphase_cn, cphase_a, cphase_b, cphase_c) VALUES ("%s","%s",%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)'%(meter['meter_id'], entry['timestamp'], entry['point'][0]['value'],entry['point'][21]['value'],entry['point'][22]['value'],entry['point'][23]['value'],entry['point'][51]['value'],entry['point'][52]['value'],entry['point'][53]['value'],entry['point'][54]['value'],entry['point'][55]['value'],entry['point'][56]['value'],entry['point'][57]['value'],entry['point'][58]['value'],entry['point'][59]['value'],entry['point'][60]['value'],entry['point'][61]['value'],entry['point'][62]['value'],entry['point'][63]['value'],entry['point'][64]['value'],entry['point'][65]['value'],entry['point'][66]['value'],entry['point'][67]['value'],entry['point'][68]['value'],entry['point'][69]['value'],entry['point'][70]['value'],entry['point'][71]['value']))
 dbs.commit();
@@ -81,14 +77,14 @@ if not sql.fetchone():
 	sql.execute('CREATE TABLE block_buildings (id int NOT NULL AUTO_INCREMENT, Name CHAR(64), block_id int, building_id int, PRIMARY KEY (id), FOREIGN KEY (block_id) REFERENCES blocks(id), FOREIGN KEY (building_id) REFERENCES buildings(id))')
 
 
-# sql.execute('CREATE INDEX meter_building ON meters (building_id)')
-# sql.execute('CREATE INDEX temp_data_meter ON temp_data (meter_address)')
-# sql.execute('CREATE INDEX data_building ON data (building_id)')
-# sql.execute('CREATE INDEX data_time ON data (time)')
-# sql.execute('CREATE INDEX bbuilding_block ON block_buildings (block_id)')
-# sql.execute('CREATE INDEX bbuilding_building ON block_buildings (building_id)')
-# sql.execute('CREATE INDEX block_dashboard ON blocks (dashboard_id)')
-# sql.execute('CREATE INDEX dashboard_story ON dashboards (story_id)')
+sql.execute('CREATE INDEX meter_building ON meters (building_id)')
+sql.execute('CREATE INDEX temp_data_meter ON temp_data (meter_address)')
+sql.execute('CREATE INDEX data_building ON data (building_id)')
+sql.execute('CREATE INDEX data_time ON data (time)')
+sql.execute('CREATE INDEX bbuilding_block ON block_buildings (block_id)')
+sql.execute('CREATE INDEX bbuilding_building ON block_buildings (building_id)')
+sql.execute('CREATE INDEX block_dashboard ON blocks (dashboard_id)')
+sql.execute('CREATE INDEX dashboard_story ON dashboards (story_id)')
 
 dbs.commit();
 dbs.close();
