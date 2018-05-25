d828e8e93559970b822592c70a0187a8889858f1
Brogan Miner <BroganMiner22@gmail.com>
2018-05-25 12:49:54 -0700

    install vue

---
+++ energy-dashboard-frontend/src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='background'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.background {
+  background-color: #424242;
+  height: 100%;
+  width: 100%;
+}
+</style>
