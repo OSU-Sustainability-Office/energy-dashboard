3b537a875309b125d9a1420ed2f24ce3dc4a3f69
Brogan Miner <BroganMiner22@gmail.com>
2018-07-18 16:57:51 -0700

    cas fix

--- energy-dashboard-frontend/config/prod.env.js
+++ energy-dashboard-frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://ec2-54-186-223-223.us-west-2.compute.amazonaws.com:3478/"'
+  ROOT_API: '"http://54.186.223.223:3478/"'
 }
