7d7239440d07eb02919e7c135ba419de4f7a313d
Brogan Miner <BroganMiner22@gmail.com>
2018-09-28 20:10:55 -0700

    get rid of extra /

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://52.39.141.177:3000/"',
+  ROOT_API: '"http://52.39.141.177:3000"',
   HOST_ADDRESS: '"https://osu-sustainability-office.github.io/energy-dashboard/"',
 }
