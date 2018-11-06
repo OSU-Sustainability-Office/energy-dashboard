8d09a09de8b497e7a2013b5069c7227d9c949a14
Brogan Miner <BroganMiner22@gmail.com>
2018-11-06 13:20:21 -0800

    add tooltip config for all charts

--- src/components/charts/piechart.js
+++ src/components/charts/piechart.js
@@ -16,6 +16,30 @@ export default {
             top: 0
           }
         },
+        tooltips: {
+          callbacks: {
+            title: function (item, data) {
+              let d = new Date(item[0].xLabel)
+              let meridiem = 'am'
+              let hours = d.getHours()
+              if (hours > 12) {
+                hours -= 12
+                meridiem = 'pm'
+              } else if (hours === 0) {
+                hours = 12
+              }
+              let minutes = d.getMinutes()
+              if (minutes < 10) {
+                minutes = '0' + minutes
+              }
+              let year = d.getYear().toString().slice(1)
+              return (d.getMonth() + 1).toString() + '/' + d.getDate() + '/' + year + ' ' + hours + ':' + minutes + ' ' + meridiem
+            },
+            label: (item, data) => {
+              return item.yLabel + ' ' + this.$parent.unit()
+            }
+          }
+        },
         legend: {
           labels: {
             fontColor: 'white'
