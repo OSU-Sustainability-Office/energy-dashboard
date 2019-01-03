5e64cc6e62dff71aea30512a2e8e7d54d9db9bc2
Brogan Miner <BroganMiner22@gmail.com>
2019-01-03 12:46:07 -0800

    Fix logout route

--- src/store/api.js
+++ src/store/api.js
@@ -3,7 +3,7 @@
  * @Date:   2018-12-20T16:22:04-08:00
  * @Email:  brogan.miner@oregonstate.edu
  * @Last modified by:   Brogan
- * @Last modified time: 2018-12-20T18:30:24-08:00
+ * @Last modified time: 2019-01-03T12:40:38-08:00
  */
 import axios from 'axios'
 
@@ -65,6 +65,6 @@ export default {
     return (await callAPI('alert', data, method)).data
   },
   logout: async () => {
-    return (await callAPI('logout')).data
+    return (await callAPI('logout', null, 'get', '/auth/')).data
   }
 }
