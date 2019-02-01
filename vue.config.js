a03b25cc0732160fa20a9b99a447086fe976e083
Brogan Miner <BroganMiner22@gmail.com>
2019-02-01 14:10:26 -0800

    Speed up build times

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Date:   2019-01-09T13:26:49-08:00
  * @Email:  brogan.miner@oregonstate.edu
  * @Last modified by:   Brogan
- * @Last modified time: 2019-01-09T13:33:15-08:00
+ * @Last modified time: 2019-02-01T14:07:17-08:00
  */
 
 const path = require('path')
@@ -21,6 +21,26 @@ module.exports = {
       preProcessor: 'scss',
       patterns: [
         path.resolve(__dirname, 'src/assets/style-variable.scss')
+      ]
+    }
+  },
+  configureWebpack: {
+    optimization: {
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            'cache-loader',
+            'thread-loader',
+            'babel-loader'
+          ]
+        }
       ]
     }
   }
