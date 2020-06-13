3cee43d7e9346f0305ed9664c63d618fdc9c840e
obergad <obergad@oregonstate.edu>
2020-06-13 16:56:30 -0700

    fixed comments from jack and brogan

---
+++ src/components/kiosk/embeddable_page.vue
@@ -0,0 +1,63 @@
+<!--
+@Author: Adam Oberg <Aadm
+@Date:   2020-05-06 T:22:00:00-22:30:00
+@Email:  adam.oberg@oregonstate.edu
+@Last modified by:   Adam
+@Last modified time: 2020-05-06 T:22:00:00-22:30:00
+-->
+
+<template>
+  <el-row class='stage'>
+    <el-col>
+      Hello World.
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+export default {
+  name: 'embeddable_page',
+  data () {
+  },
+  beforeDestroy () {
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+
+</script>
+
+<style scoped lang='scss'>
+@import '@/assets/style-variables.scss';
+
+.app {
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 100%;
+}
+.header {
+  margin: 0;
+  padding: 0;
+  height: $--nav-height;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.main {
+  position: absolute;
+  top: $--nav-height;
+  left: 0;
+  min-height: calc(100% - #{$--nav-height});
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ import campaigns from '@/components/campaigns/campaign_list'
 import notfound from '@/components/extras/404.vue'
 import mainCampaignView from '@/components/campaigns/main_campaign_view.vue'
 import buildingList from '@/components/building_list/building_list.vue'
-import extras from '@/components/extras/embeddable-page.vue'
+import kiosks from '@/components/kiosk/embeddable_page.vue'
 // import admin from '@/components/admin/admin.vue'
 
 Vue.use(Router)
@@ -70,8 +70,8 @@ export default new Router({
       component: buildingList
     },
     {
-      path: '/embeddable-page',
-      component: extras
+      path: '/kiosks',
+      component: kiosks
     },
     {
       path: '*',
