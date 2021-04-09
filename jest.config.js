929fe79c8c802738cc311895ae5e2f43f7e16c76
MilanDonhowe <milanaugust@gmail.com>
2021-04-09 13:45:34 -0700

    Updated Jest Vuecli-Plugin

--- jest.config.js
+++ jest.config.js
@@ -200,5 +200,6 @@ module.exports = {
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
 
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  preset: '@vue/cli-plugin-unit-jest'
 }
