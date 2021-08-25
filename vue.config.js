044e604d2e186c751d2c7f681688a9d771721ef5
MilanDonhowe <milanaugust@gmail.com>
2021-08-25 14:09:29 -0700

    Fixed data handlingg (now points should work again)

--- vue.config.js
+++ vue.config.js
@@ -26,6 +26,16 @@ module.exports = {
   },
   chainWebpack: config => {
     config.module.rules.delete('svg')
+    // remove comments from vue build (shoutout to @tony19  https://stackoverflow.com/a/61768334/8638218)
+    /* Doesn't work yet since we're using uglify-js instead of terset still on this version of vue-cli
+    config.optimization.minimizer('terser').tap((args) => {
+      args[0].terserOptions.output = {
+        ...args[0].terserOptions.output,
+        comments: false
+      }
+      return args
+    })
+    */
   },
   configureWebpack: {
     optimization: {
