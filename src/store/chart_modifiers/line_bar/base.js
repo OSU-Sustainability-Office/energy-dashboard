f9d5a7686e9decc84c6ec4d82fdcce6372831c83
solderq35 <solderq35@gmail.com>
2022-12-14 20:39:51 -0800

    monthly fix

--- src/store/chart_modifiers/line_bar/base.js
+++ src/store/chart_modifiers/line_bar/base.js
@@ -1,12 +1,11 @@
-/*
- * @Author: you@you.you
- * @Date:   Wednesday March 25th 2020
- * @Last Modified By:  Brogan Miner
- * @Last Modified Time:  Wednesday March 25th 2020
- * @Copyright:  (c) Oregon State University 2020
- */
-
-export default class LineBaseModifier {
+/**
+  Filename: accumulated_real.js
+  Info: Chart math & setup for accumulated real meter point.
+*/
+export default class LineAccumulatedModifier {
+  constructor () {
+    this.data = {}
+  }
   /*
     Description: Called after getData function of chart module. Create
     a new class following this template if a new modifier type is needed
@@ -30,9 +29,8 @@ export default class LineBaseModifier {
           dateEnd: epoch time in seconds of graph end (integer)
           intervalUnit: unit of interval to group data points by (string: 'minute', 'hour', 'day')
           dateInterval: count of interval units to group data points by (integer)
-          timezoneOffset*: timezone of initial timestamp (optional)
         }
-      - store (vuex store)
+      - store (Vuex store)
       - module: (vuex module) module dispatching this function call
 
     Returns: Nothing (Note: chartData is passed by reference so editiing this argument will change it in the chart update sequence)
@@ -41,6 +39,8 @@ export default class LineBaseModifier {
     let resultDataObject = chartData.data
     let returnData = []
     let delta = 1
+    let startDate = (new Date((payload.dateStart) * 1000))
+    let monthDays = 1
     switch (payload.intervalUnit) {
       case 'minute':
         delta = 60
@@ -51,20 +51,46 @@ export default class LineBaseModifier {
       case 'day':
         delta = 86400
         break
+      case 'month':
+        monthDays = (new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0)).getDate()
+        delta = 60 * 60 * 24 * monthDays
+        break
     }
     delta *= payload.dateInterval
-
-    // set the offset if there is one we need to account for
-    const offset = (payload.timeZoneOffset) ? payload.timeZoneOffset : 0
+    payload.dateStart = payload.dateStart - (payload.dateStart % 900)
+    payload.dateEnd = payload.dateEnd - (payload.dateEnd % 900)
 
     for (let i = payload.dateStart; i <= payload.dateEnd; i += delta) {
+      let oldDate = (new Date(i * 1000))
+      if (payload.intervalUnit === 'month') {
+        let monthDaysCurrent = (new Date(oldDate.getFullYear(), oldDate.getMonth() + 1, 0)).getDate()
+
+        delta += (monthDaysCurrent - monthDays) * 24 * 60 * 60
+        monthDays = monthDaysCurrent
+      }
+      let dataDate = (new Date((i + delta) * 1000))
       try {
         let accumulator = 0
         if (isNaN(resultDataObject.get(i + delta)) || isNaN(resultDataObject.get(i))) {
           continue
         }
-        accumulator = resultDataObject.get(i + delta)
-        returnData.push({ x: (new Date((i + delta + offset) * 1000)), y: accumulator })
+        if (Math.abs(resultDataObject.get(i + delta)) < Math.abs(resultDataObject.get(i))) {
+          continue
+        }
+        // If either reading is zero that indicates a missing reading -- do not report.
+        if (resultDataObject.get(i + delta) === 0 || resultDataObject.get(i) === 0) {
+          continue
+        }
+        accumulator = resultDataObject.get(i + delta) - resultDataObject.get(i)
+
+        if (payload.point === 'total') {
+          // Steam meters report in 100s of lbs
+          accumulator *= 100
+        }
+        // While some readings are negative for offset purposes, we should
+        // still display them as positive readings since negative electricity
+        // isn't really what our meters should detect.
+        returnData.push({ x: dataDate, y: Math.abs(accumulator) })
       } catch (error) {
         console.log(error)
       }
@@ -86,11 +112,31 @@ export default class LineBaseModifier {
           intervalUnit: unit of interval to group data points by (string: 'minute', 'hour', 'day')
           dateInterval: count of interval units to group data points by (integer)
         }
-      - store (vuex store)
+      - store (Vuex store)
       - module: (vuex module) module dispatching this function call
 
     Returns: Nothing (Note: payload is passed by reference so editiing this argument will change it in the chart update sequence)
   */
   async preGetData (payload, store, module) {
+    let delta = 1
+    let dataDate = (new Date((payload.dateStart) * 1000))
+    switch (payload.intervalUnit) {
+      case 'minute':
+        delta = 60
+        break
+      case 'hour':
+        delta = 3600
+        break
+      case 'day':
+        delta = 86400
+        break
+      case 'month':
+        let monthDays = (new Date(dataDate.getFullYear(), dataDate.getMonth(), 0)).getDate()
+        if (dataDate.getDate() > monthDays) monthDays = dataDate.getDate()
+        delta = 60 * 60 * 24 * monthDays
+        break
+    }
+    delta *= payload.dateInterval
+    payload.dateStart = (payload.dateStart -  delta) - (payload.dateStart % 900)
   }
 }
