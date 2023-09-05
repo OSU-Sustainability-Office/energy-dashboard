c1d76fae76e075bc0dd37bf30889468de75fa2cc
solderq35 <solderq35@gmail.com>
2023-09-04 20:26:22 -0700

    probably won't work but let's see

--- backend/tests/data_layer.spec.js
+++ backend/tests/data_layer.spec.js
@@ -104,6 +104,67 @@ describe('Testing data_layer related API endpoints...', () => {
     expect(jsonData['data'][1]['readings'].length).toBeGreaterThan(10)
 
   })
+  const meter_id = 'Solar_Meters'
+  it('mock solar data upload...', async () => {
+    process.env.ACQUISUITE_PASS = 'test_pwd'
+    // Have to make meter_id acceptable for SQL commands
+    const mockRequest = {
+      headers: {
+        ...MOCK_REQUEST_EVENT.headers
+      },
+      body: JSON.stringify({
+        id: meter_id,
+        body: solarData,
+        pwd: process.env.ACQUISUITE_PASS,
+        type: 'solar'
+      })
+    }
+    // Check that we can upload data
+    let response = await MeterData.upload(mockRequest, undefined)
+    expect(response.statusCode).toBe(200)
+    // Check that data was written
+    let energy_data = await DB.query('SELECT * from ' + meter_id)
+    expect(energy_data.length).toBe(solarData.length)
+    // Check that meter was added to Meters table
+    const [{ 'id': meter_normalized_id }] = await DB.query('SELECT id FROM meters WHERE name = ?;', [meter_id])
+
+    // Check that we can GET data
+    const mockReadRequest = {
+      headers: {
+        origin: `${client.scheme}://${client.host}`
+      },
+      queryStringParameters: {
+        'id': meter_normalized_id,
+        'point': 'total_energy',
+        'startDate': 1693785599,
+        'endDate': 1693785600,
+        'meterClass': 9990001
+      }
+    }
+    let EnergyResponse = await MeterData.data(mockReadRequest)
+    const meter_data = JSON.parse(EnergyResponse.body)
+    expect(meter_data.length).toBe(solarData.length)
+    expect(meter_data[0]['total_energy']).toBe(3665740)
+    // Make sure batch route works too
+    const batchRequest = {
+      headers: {
+        ...MOCK_REQUEST_EVENT.headers
+      },
+      body: JSON.stringify({
+        point: 'total_energy',
+        meterClass: 9990001,
+        datasets: [
+          { id: meter_normalized_id, startDate: 1627974000, endDate: 1627976700 }
+        ]
+      })
+    }
+
+    response = await MeterData.batchData(batchRequest)
+    const jsonData = JSON.parse(response.body)
+    console.log(jsonData)
+    expect(jsonData['data'][0]['readings'].length).toBe(solarData.length)
+    expect(jsonData['data'][0]['readings'][0]['reading']).toBe(3665740)
+  })
 /*
   const meter_id = 'M' + '007c9349-72ba-450c-aa1f-4e5a77b68f79'.replace(/-/g, 'M')
   it('mock solar data upload...', async () => {
