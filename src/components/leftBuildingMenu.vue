9a933381536bcb838a1c1993de9faefe18d33723
solderq35 <solderq35@gmail.com>
2023-09-13 21:54:40 -0700

    hide menu button styling fix

--- src/components/leftBuildingMenu.vue
+++ src/components/leftBuildingMenu.vue
@@ -47,5 +47,16 @@ export default {
 .toggleButton {
   cursor: pointer;
   font-size: 15px;
+  background-color: white;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial,
+    sans-serif;
+  display: flex;
+  align-items: center;
+  border: 2px solid rgba(0, 0, 0, 0.2);
+  background-clip: padding-box;
+  border-radius: 4.5px;
+  opacity: 1;
+  justify-content: center;
+  z-index: 500;
 }
 </style>
--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -670,20 +670,10 @@ $sideMenu-width: 250px;
 }
 
 .hideMenuButton {
-  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial,
-    sans-serif;
   display: flex;
-  align-items: center;
   position: absolute;
   top: 65px;
   left: 50px;
-  background-color: white;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  background-clip: padding-box;
-  border-radius: 4.5px;
-  opacity: 1;
-  justify-content: center;
-  z-index: 500;
 }
 
 .side-enter-active,
