d83209f5840215b07b86c475786f729bdb4bd2d0
solderq35 <solderq35@gmail.com>
2023-09-13 19:06:04 -0700

    0.004

--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -281,7 +281,7 @@ export default {
     getResult ( searchResult ) {
       console.log( searchResult.getBounds().getCenter() )
       let searchLatLng = searchResult.getBounds().getCenter()
-      searchLatLng.lng = searchLatLng.lng - 0.006
+      searchLatLng.lng = searchLatLng.lng - 0.004
       console.log( searchLatLng )
       this.map.setView( L.latLng( searchLatLng ), 15.5 )
       console.log( searchResult.feature.properties.name )
