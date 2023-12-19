1af7a01fb522a42174355b5d31105412e31490f7
solderq35 <solderq35@gmail.com>
2023-12-19 12:29:40 -0800

    update comments

--- src/components/view/view.vue
+++ src/components/view/view.vue
@@ -85,11 +85,13 @@ export default {
           if (this.cards.length > 0 && this.cards[0]) {
             console.log(buildings.map(building => building.id))
             await this.$store.dispatch(this.cards[0].path + '/removeAllModifiers')
-            // addModifier and updateModifier below call src\store\block_modifiers\building_compare.mod.js
+            // addModifier and updateModifier below call block.module.js, then building_compare.mod.js
+            // See addModifier and updateModifier functions in block.module.js code
             // and src\store\block.module.js to add new charts to comparison / remove old charts (duplicate chart path triggers error)
             await this.$store.dispatch(this.cards[0].path + '/addModifier', 'building_compare')
 
-            // buildingIds below defines which buildingId are sent to building_compare.mod.js, which also affects chartSpace naming
+            // buildingIds below defines which buildingId are sent to block.module.js and then building_compare.mod.js,
+            // which also affects chartSpace naming
             await this.$store.dispatch(this.cards[0].path + '/updateModifier', {
               name: 'building_compare',
               data: {
