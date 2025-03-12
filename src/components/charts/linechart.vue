e29125ebecee9ad12ecbb6892dbe238fd8b6ca6f
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-03-11 17:40:09 -0700

    Refractored linechart.vue

--- src/components/charts/linechart.vue
+++ src/components/charts/linechart.vue
@@ -48,156 +48,15 @@ export default {
     options: function () {
       return {
         devicePixelRatio: 2,
-        datasets: {
-          line: {
-            tension: 0.4
-          }
-        },
-        plugins: {
-          title: {
-            color: this.primaryColor,
-            font: {
-              size: 12,
-              family: 'Open Sans'
-            }
-          },
-          legend: {
-            labels: {
-              color: this.primaryColor,
-              font: {
-                size: 12,
-                family: 'Open Sans'
-              }
-            },
-            onHover: e => {
-              e.native.target.style.cursor = 'pointer'
-            },
-            onLeave: e => {
-              e.native.target.style.cursor = 'default'
-            }
-          },
-          tooltip: {
-            titleColor: '#FFFFFF',
-            bodyColor: '#FEFEFE',
-            titleFont: { size: 12, family: 'Open Sans' },
-            bodyFont: { size: 12, family: 'Open Sans' },
-            callbacks: {
-              title: function (tooltipItems) {
-                // use originalXlabel if it exists, otherwise use the default xLabel
-                const originalXlabel = tooltipItems[0].parsed.x
-                const d = new Date(originalXlabel || tooltipItems[0].label)
-                const dt = DateTime.fromJSDate(d)
-                return dt.toFormat('ccc MM/dd/yy h:mm a')
-              },
-              label: function (tooltipItem) {
-                const yLabel = tooltipItem.parsed.y
-                const label = tooltipItem.dataset.label
-                const unit = tooltipItem.dataset.unit
-                return label + ': ' + parseFloat(yLabel).toFixed(2) + ' ' + unit
-              }
-            }
-          }
-        },
-        elements: {
-          point: {
-            radius: 3
-          }
-        },
-        layout: {
-          padding: {
-            left: 0,
-            right: 0,
-            bottom: 0,
-            top: 0
-          }
-        },
-        interaction: {
-          mode: 'index',
-          intersect: true
-        },
-        hover: {
-          onHover (event, chartElement) {
-            if (chartElement.length) {
-              event.native.target.style.cursor = 'pointer'
-            } else {
-              event.native.target.style.cursor = 'default'
-            }
-          }
-        },
+        datasets: this.getDatasetOptions(),
+        plugins: this.getPluginOptions(),
+        elements: this.getElementOptions(),
+        layout: this.getLayoutOptions(),
+        interaction: this.getInteractionOptions(),
+        hover: this.getHoverOptions(),
         responsive: true,
         maintainAspectRatio: false,
-        scales: {
-          y: {
-            beginAtZero: false,
-            ticks: {
-              font: {
-                size: 12,
-                family: 'Open Sans'
-              },
-              color: this.primaryColor,
-              autoSkip: true,
-              maxTicksLimit: 10,
-              callback: (val, index) => {
-                return val.toString()
-              }
-            },
-            grid: {
-              display: true,
-              color: this.primaryColor
-            },
-            border: {
-              color: this.primaryColor
-            },
-            title: {
-              display: this.buildLabel('y') !== '',
-              text: this.buildLabel('y'),
-              color: this.primaryColor,
-              font: {
-                size: 12,
-                family: 'Open Sans'
-              }
-            }
-          },
-          x: {
-            type: 'time',
-            bounds: 'data',
-            grid: {
-              display: false
-            },
-            ticks: {
-              source: 'data',
-              font: {
-                size: 14,
-                family: 'Open Sans'
-              },
-              color: this.primaryColor,
-              autoSkip: true,
-              // the following three settings change the x-ticks if there are multiple time periods,
-              // otherwise the default settings are used
-              autoSkipPadding: this.isMultipleTimePeriods ? 15 : 8,
-              maxRotation: this.isMultipleTimePeriods ? 0 : 50,
-              callback: (val, index) => {
-                if (this.isMultipleTimePeriods) {
-                  return this.buildXaxisTick(index)
-                }
-                return this.formatXaxisTick(val)
-              }
-            },
-
-            title: {
-              display: this.buildLabel('x') !== '',
-              text: this.buildLabel('x'),
-              color: this.primaryColor,
-              font: {
-                size: 12,
-                family: 'Open Sans'
-              }
-            },
-            time: {
-              unit: this.getIntervalUnit()
-            }
-          }
-        }
+        scales: this.getScaleOptions()
       }
     }
   },
@@ -244,6 +103,168 @@ export default {
       const days = dtEnd.diff(dtStart, 'days').days
 
       return days
+    },
+    getDatasetOptions: function () {
+      return {
+        line: {
+          tension: 0.4
+        }
+      }
+    },
+    getPluginOptions: function () {
+      return {
+        title: {
+          color: this.primaryColor,
+          font: {
+            size: 12,
+            family: 'Open Sans'
+          }
+        },
+        legend: {
+          labels: {
+            color: this.primaryColor,
+            font: {
+              size: 12,
+              family: 'Open Sans'
+            }
+          },
+          onHover: e => {
+            e.native.target.style.cursor = 'pointer'
+          },
+          onLeave: e => {
+            e.native.target.style.cursor = 'default'
+          }
+        },
+        tooltip: {
+          titleColor: '#FFFFFF',
+          bodyColor: '#FEFEFE',
+          titleFont: { size: 12, family: 'Open Sans' },
+          bodyFont: { size: 12, family: 'Open Sans' },
+          callbacks: {
+            title: function (tooltipItems) {
+              // use originalXlabel if it exists, otherwise use the default xLabel
+              const originalXlabel = tooltipItems[0].parsed.x
+              const d = new Date(originalXlabel || tooltipItems[0].label)
+              const dt = DateTime.fromJSDate(d)
+              return dt.toFormat('ccc MM/dd/yy h:mm a')
+            },
+            label: function (tooltipItem) {
+              const yLabel = tooltipItem.parsed.y
+              const label = tooltipItem.dataset.label
+              const unit = tooltipItem.dataset.unit
+              return label + ': ' + parseFloat(yLabel).toFixed(2) + ' ' + unit
+            }
+          }
+        }
+      }
+    },
+    getElementOptions: function () {
+      return {
+        point: {
+          radius: 3
+        }
+      }
+    },
+    getLayoutOptions: function () {
+      return {
+        padding: {
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0
+        }
+      }
+    },
+    getInteractionOptions: function () {
+      return {
+        mode: 'index',
+        intersect: true
+      }
+    },
+    getHoverOptions: function () {
+      return {
+        onHover (event, chartElement) {
+          if (chartElement.length) {
+            event.native.target.style.cursor = 'pointer'
+          } else {
+            event.native.target.style.cursor = 'default'
+          }
+        }
+      }
+    },
+    getScaleOptions: function () {
+      return {
+        y: {
+          beginAtZero: false,
+          ticks: {
+            font: {
+              size: 12,
+              family: 'Open Sans'
+            },
+            color: this.primaryColor,
+            autoSkip: true,
+            maxTicksLimit: 10,
+            callback: (val, index) => {
+              return val.toString()
+            }
+          },
+          grid: {
+            display: true,
+            color: this.primaryColor
+          },
+          border: {
+            color: this.primaryColor
+          },
+          title: {
+            display: this.buildLabel('y') !== '',
+            text: this.buildLabel('y'),
+            color: this.primaryColor,
+            font: {
+              size: 12,
+              family: 'Open Sans'
+            }
+          }
+        },
+        x: {
+          type: 'time',
+          bounds: 'data',
+          grid: {
+            display: false
+          },
+          ticks: {
+            source: 'data',
+            font: {
+              size: 14,
+              family: 'Open Sans'
+            },
+            color: this.primaryColor,
+            autoSkip: true,
+            // the following three settings change the x-ticks if there are multiple time periods,
+            // otherwise the default settings are used
+            autoSkipPadding: this.isMultipleTimePeriods ? 15 : 8,
+            maxRotation: this.isMultipleTimePeriods ? 0 : 50,
+            callback: (val, index) => {
+              if (this.isMultipleTimePeriods) {
+                return this.buildXaxisTick(index)
+              }
+              return this.formatXaxisTick(val)
+            }
+          },
+
+          title: {
+            display: this.buildLabel('x') !== '',
+            text: this.buildLabel('x'),
+            color: this.primaryColor,
+            font: {
+              size: 12,
+              family: 'Open Sans'
+            }
+          },
+          time: {
+            unit: this.getIntervalUnit()
+          }
+        }
+      }
     }
   }
 }
