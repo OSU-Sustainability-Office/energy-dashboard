3fcac6666cbb9ec764d57b5a308b9a26b3bc25b2
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-04-12 16:09:04 -0700

    Remove vue-i18n

--- src/main.js
+++ src/main.js
@@ -10,18 +10,11 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import ElementPlus from 'element-plus'
-import { createI18n } from 'vue-i18n'
-
-const i18n = createI18n({
-  locale: 'en',
-  messages: {}
-})
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
 app.use(ElementPlus)
-app.use(i18n)
 
 app.mount('#app')
