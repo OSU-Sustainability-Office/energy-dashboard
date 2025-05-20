60857d2869c1db54a21edf398841b07f210179e8
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-05-20 12:32:44 -0700

    Adding points to index.js

--- src/store/chart_modifiers/index.js
+++ src/store/chart_modifiers/index.js
@@ -11,30 +11,91 @@ import LineDefault from './line_bar/default.js'
 import LineTotalBaseline from './line_bar/baseline_total.js'
 import LineTotalPercModifier from './line_bar/baseline_perc_total.js'
 import LinePeriodicReal from './line_bar/periodic_real.js'
+import LineMinMaxModifier from './line_bar/min_max.js'
+import LineAverageModifier from './line_bar/average.js'
 
 export default function (graphType, point) {
   if (graphType === 1 || graphType === 2) {
     // Line or Bar
     switch (point) {
-      case 'accumulated_real':
+      case 'accumulated_real': // Acquasuite
         return new LineAccumulatedReal()
-      case 'total':
+      case 'total': // Acquasuite
         return new LineAccumulatedReal()
-      case 'cubic_feet':
+      case 'cubic_feet': // Acquasuite
         return new LineAccumulatedReal()
-      case 'baseline_percentage':
+      case 'baseline_percentage': // Baseline Percentage for Acquasuite
         return new LineBaselinePerc()
       case 'avg_accumulated_real':
-        return new LineBaselineAvg()
+        return new LineBaselineAvg() // Baseline Average for Acquasuite
       case 'baseline_total':
-        return new LineTotalBaseline()
+        return new LineTotalBaseline() // Baseline Average for Pacific Power meters
       case 'baseline_perc_total':
-        return new LineTotalPercModifier()
+        return new LineTotalPercModifier() // Baseline Percentage Total for Pacific Power meters
       case 'periodic_real_in':
-        return new LinePeriodicReal()
+        return new LinePeriodicReal() // Similar logic to Accumulated Real but supports Pacific Power meters
       case 'periodic_real_out':
-        return new LinePeriodicReal()
+        return new LinePeriodicReal() // Similar logic to Accumulated Real but supports Pacific Power meters
+      case 'real_power':
+        return new LineMinMaxModifier()
+      case 'reactive_power':
+        return new LineMinMaxModifier()
+      case 'apparent_power':
+        return new LineMinMaxModifier()
+      case 'real_a':
+        return new LineMinMaxModifier()
+      case 'real_b':
+        return new LineMinMaxModifier()
+      case 'real_c':
+        return new LineMinMaxModifier()
+      case 'reactive_a':
+        return new LineMinMaxModifier()
+      case 'reactive_b':
+        return new LineMinMaxModifier()
+      case 'reactive_c':
+        return new LineMinMaxModifier()
+      case 'cphase_a':
+        return new LineMinMaxModifier()
+      case 'cphase_b':
+        return new LineMinMaxModifier()
+      case 'cphase_c':
+        return new LineMinMaxModifier()
+      case 'apparent_a':
+        return new LineMinMaxModifier()
+      case 'apparent_b':
+        return new LineMinMaxModifier()
+      case 'apparent_c':
+        return new LineMinMaxModifier()
+      case 'input':
+        return new LineMinMaxModifier()
+      case 'maximum':
+        return new LineMinMaxModifier()
+      case 'instant':
+        return new LineMinMaxModifier()
+      case 'rate':
+        return new LineMinMaxModifier()
+      case 'pf_a':
+        return new LineMinMaxModifier()
+      case 'pf_b':
+        return new LineMinMaxModifier()
+      case 'pf_c':
+        return new LineMinMaxModifier()
+      case 'minimum':
+        return new LineMinMaxModifier()
+      case 'vphase_ab':
+        return new LineAverageModifier()
+      case 'vphase_ac':
+        return new LineAverageModifier()
+      case 'vphase_bc':
+        return new LineAverageModifier()
+      case 'vphase_an':
+        return new LineAverageModifier()
+      case 'vphase_bn':
+        return new LineAverageModifier()
+      case 'vphase_cn':
+        return new LineAverageModifier()
       default:
+        console.error('Unknown point type for line/bar chart:', point)
         return new LineDefault()
     }
   } else {
