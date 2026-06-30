82530ccb3b45a9807435dacfd5143feff4249bfa
Stanley Eng <stanleyeng665@gmail.com>
2026-06-30 16:06:05 -0700

    Enforce LF line endings in prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,6 @@ export default {
   trailingComma: 'none',
   arrowParens: 'avoid', // parentheses around arrow function parameters only when necessary
   tabWidth: 2, // matches ESLint's indent rule
-  semi: false
+  semi: false,
+  endOfLine: 'lf'
 }
